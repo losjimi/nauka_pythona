22a110706d7579cf5d6062506038f7f3cf8494a4
losjimi <losjimi@users.noreply.github.com>
2018-12-09 12:17:55 +0100

    podwojne listy

--- listy.py
+++ listy.py
@@ -1,5 +1,7 @@
 samochody=['maluch','syrenka','porsze','polojnezo']
 lody=['bakaliowe','pistacjowe','jagodowe','czekoladowe','miodowe']
+ilosc=[1, 3, 5, 7]
+litry=[3, 2, 6, 8, 99]
 
 
 print(samochody[0])
@@ -19,11 +21,25 @@ for idx in range(len(samochody)):
 print(lody[3])
 for kolejnosc in range(len(lody)):
     print('Kolejnosc tworzenia lodow:' + str(kolejnosc) + ' i ich smak: ' + lody[kolejnosc])
-
-s='-'
-print(s.join(lody)) #laczenie wartosci z listy
-
 #ostatni element listy
 print(lody[-1])
 #pierwszy i ostatni
 print(lody[0:-1])
+
+
+s='-'
+print(s.join(lody)) #laczenie wartosci z listy
+#lub print('-'.join(lody))
+arr='a,b,c,d,e'.split(',')
+print(arr)
+
+#kasowanie pozycji del samochody[0]
+#nadpisanie samochody[1] = 'kia'
+
+#zad 11 samochody i ilosc_osob
+for idx  in range(len(samochody)):
+    print('idx: ' + str(idx) + ' : ' + samochody[idx])
+    print (samochody[idx] + ' ma ilosc drzwi = ' + str(ilosc[idx]))
+
+for a in range(len(lody)):
+    print(str(a) + ' : ' + lody[a] + ' mamy ich az ' + str(litry[a]) + ' litrow.')
