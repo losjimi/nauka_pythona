8f75a505871b5c2abdf2990b8d81b5d13aaf0db3
losjimi <losjimi@users.noreply.github.com>
2018-12-09 10:13:00 +0100

    Python czas start: prezentacja textu oraz zamiana int na stringi

---
+++ drugi.py
@@ -0,0 +1,9 @@
+nazwa_publikacji = 'gazeta'
+dlugosc_publikacji = 20
+cena_dzien = 150.99
+
+print('Twoj artykul publikowany bedzie w medium jakim jest '
++ nazwa_publikacji
++ ' przez ' + str(dlugosc_publikacji) + ' dni ' + ' za kwote '
++ str(cena_dzien)
++ ' brutto')
