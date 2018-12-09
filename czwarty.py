7b76b1eb63920d05971647d9f8dde3a6ad8c28a5
losjimi <losjimi@users.noreply.github.com>
2018-12-09 11:14:46 +0100

    warunki dodatkowe

--- czwarty.py
+++ czwarty.py
@@ -5,6 +5,7 @@ ilosc_osob=3
 wiek_1=30
 wiek_2=25
 wiek_3=16
+dzien='Sobota'
 
 print('Dzisiaj bedziemy odpoczywac. Wybralismy rodzaj zabawy '
 + rodzaj_wypoczynku + '. Idziemy w ' + str(ilosc_osob) + ' osob.')
@@ -17,3 +18,12 @@ elif ((wiek_1 >= 18 and wiek_2 < 18 and wiek_3 < 18) or (wiek_1 < 18 and wiek_2 
     print('Za bilety zaplacimy ' + str(cena_normalna+cena_ulgowa*2))
 elif (wiek_1 < 18 and wiek_2 < 18 and wiek_3 < 18):
     print('Za bilety zaplacimy ' + str(cena_ulgowa*3))
+
+
+#if 18.00<cena_normalna<=25.00:
+if dzien.startswith('So'):
+    print('Promocja sobotnia 20%! ')
+    cena_normalna= cena_normalna-cena_normalna*0.2
+    print('Bilet w promocji: ' + str(cena_normalna))
+else:
+    print('Bilety w cenie standard - dzis nie Sobota')
